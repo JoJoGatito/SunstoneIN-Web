4d67685d878c2f1407d3cee92181b091e359dac8
JoJoGatito <jckurutteru@gmail.com>
2025-03-17 16:53:33 -0600

    omg it worked

--- board-portal/.netlify/build/entry.mjs
+++ board-portal/.netlify/build/entry.mjs
@@ -1,7 +1,7 @@
 import { renderers } from './renderers.mjs';
 import { a as actions } from './chunks/_noop-actions_CfKMStZn.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_pVKY3Dp8.mjs';
+import { manifest } from './manifest_s8ZDTxAs.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -35,7 +35,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_astro-internal_middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "a7a05134-26b5-41a9-9b7c-785c2b5bc2a4"
+    "middlewareSecret": "c415f9d3-4130-4fa2-847f-d3765bae2f02"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
