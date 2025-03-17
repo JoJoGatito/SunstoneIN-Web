644b0cee1f08324b4ce08124e3a7bb1aa0d8b22a
JoJoGatito <jckurutteru@gmail.com>
2025-03-17 12:03:20 -0600

    updated the entry.js file to use dynamic import

--- board-portal/netlify/functions/entry.js
+++ board-portal/netlify/functions/entry.js
@@ -1,1 +1,5 @@
-export { handler } from '../../dist/server/entry.mjs';
+// ES Module import is required instead of CommonJS require
+export const handler = async (event, context) => {
+  const { handler } = await import('../../dist/server/entry.mjs');
+  return handler(event, context);
+};
